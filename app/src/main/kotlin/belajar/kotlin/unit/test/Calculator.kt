f5591e79ff79cafdc83f6e95d363be16801a7615
setianjay <hari.setiaji2000@gmail.com>
2021-02-15 16:34:34 +0700

    assertEquals and assertThrows

--- app/src/main/kotlin/belajar/kotlin/unit/test/Calculator.kt
+++ app/src/main/kotlin/belajar/kotlin/unit/test/Calculator.kt
@@ -1,6 +1,16 @@
 package belajar.kotlin.unit.test
 
+import java.lang.IllegalArgumentException
+
 class Calculator {
 
     fun plus(paramsFirst: Int, paramsSecond: Int): Int = paramsFirst + paramsSecond
+
+    fun divide(paramsFirst: Int, paramsSecond: Int): Int {
+        if (paramsSecond == 0) {
+            throw IllegalArgumentException("Can't divide by zero")
+        } else {
+            return paramsFirst / paramsSecond
+        }
+    }
 }
--- app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -1,17 +1,35 @@
 package belajar.kotlin.unit.test
 
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class CalculatorTest {
     val calculator = Calculator()
 
     @Test
     fun testPlusSuccess(){
-        calculator.plus(10,20)
+        val result = calculator.plus(10,10)
+        assertEquals(20,result,"Hasil harusnya 30")
     }
 
     @Test
     fun testPlusSuccess2(){
-        calculator.plus(10,20)
+        val result = calculator.plus(10,20)
+        assertEquals(30,result,"Hasil harusnya 30")
+    }
+
+    @Test
+    fun testDivideSuccess(){
+        val result = calculator.divide(20,10)
+        assertEquals(2,result,"Hasil harusnya")
+    }
+
+    @Test
+    fun testDivideError(){
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(20,0)
+        }
     }
 }
