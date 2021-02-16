09fd6d2b318081c55f57fc72773647f253cd0533
setianjay <hari.setiaji2000@gmail.com>
2021-02-16 12:49:05 +0700

    BeforeEach and AfterEach

--- app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -13,15 +13,33 @@ import java.lang.IllegalArgumentException
 *
 * @Disabled = Menonaktifkan Test apabila method atau function di rasa masih dalam pengerjaan,
 * namun akan tetap masuk report(laporan) bahwa function ini masih dalam tahap pengerjaan.
+*
+* @BeforeEach = fungsi yang mana dieksekusi sebelum Test. BeforeEach akan dijalankan apabila Test
+* dijalankan.
+*
+* @AfterEach = fungsi yang mana dieksekusi sesudah Test. AfterEach akan dijalankan apabila test
+* dijalankan.
+*
 * */
 
 @DisplayName("Test for Class Calculator")
 class CalculatorTest {
     val calculator = Calculator()
 
+    @BeforeEach
+    fun beforeEach(){
+        println("Sebelum test")
+    }
+
+    @AfterEach
+    fun afterEach(){
+        println("Sesudah Test")
+    }
+
     @DisplayName("Test for Method plus(10,10)")
     @Test
     fun testPlusSuccess(){
+        println("testPlusSuccess() dijalankan")
         val result = calculator.plus(10,10)
         assertEquals(20,result,"Hasil harusnya 30")
     }
@@ -29,18 +47,21 @@ class CalculatorTest {
     @DisplayName("Test for Method plus(10,20)")
     @Test
     fun testPlusSuccess2(){
+        println("testPlusSuccess2() dijalankan")
         val result = calculator.plus(10,20)
         assertEquals(30,result,"Hasil harusnya 30")
     }
 
     @Test
     fun testDivideSuccess(){
+        println("testDivideSuccess() dijalankan")
         val result = calculator.divide(20,10)
         assertEquals(2,result,"Hasil harusnya")
     }
 
     @Test
     fun testDivideError(){
+        println("testDivideError() dijalankan")
         assertThrows<IllegalArgumentException> {
             calculator.divide(20,0)
         }
